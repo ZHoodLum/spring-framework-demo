d0d2108904da0b83d0f56e2b609d86c1535faddb
ZDY <1553230605@qq.com>
2021-10-12 06:52:19 +0800

    测试练习创建Bean

--- src/test/java/IocSetValueTest.java
+++ src/test/java/IocSetValueTest.java
@@ -7,7 +7,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 /**
  * @author：ZDY
  * @Date：Created on 2021/10/9 14:52
- * @Description:模拟从容器中获取组件信息
+ * @Description:模拟从容器中获取组件信息 Bean对象赋值方式
  */
 public class IocSetValueTest {
     //ApplicationContext 代表IOC容器
@@ -21,7 +21,7 @@ public class IocSetValueTest {
     @Test
     public void gatewayTest(){
         //上面的容器已经帮我们创建好了！！！
-        GatewayInfo gatewayInfo = (GatewayInfo) ioc.getBean("gatewayInfo7");
+        GatewayInfo gatewayInfo = (GatewayInfo) ioc.getBean("gatewayInfo9");
         System.out.println(gatewayInfo.toString());
     }
 
